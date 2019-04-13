a0f8ab1b249b3748be897c071be0c6f5bcb7a748
steve-ng562 <sjng1@uci.edu>
2019-04-13 14:44:45 -0700

    functionality avail cars

--- javascript/dashboard.js
+++ javascript/dashboard.js
@@ -1,8 +1,59 @@
 var map;
-var userLatitude;
-var userLongitude;
+var userLatitude = 34.040800499999996;
+var userLongitude = -118.2557107;
+var availableCars;
 
+
+getAvailableCars();
 getLocation();
+function getAvailableCars(){
+  console.log("Session storage: " + sessionStorage.getItem('user'));
+$.ajax({
+    url: 'https://34.212.86.167:80/available_cars',
+    type: 'GET',
+    data: 
+    {
+    },
+    dataType: 'json',
+    success: function(response) {
+      console.log(response);
+      availableCars = response;
+
+      var divCol  = "<div class='col-sm-4 col-md-4'>";
+        
+        var divClose= "</div>";
+
+        for(var prop in response) {
+          console.log(response[prop].info);
+           var make     = "<h3>"      + response[prop].info.make + " " + response[prop].info.model  + " " + 
+            response[prop].info.year + "</h3>";
+            var odometer = "<h3>" + response[prop].odometer.data.distance + " miles driven. </h3>";
+
+            var linkStart = "<a href='" + response[prop] + "'>";
+            var image     = "RENT NOW"
+            var linkEnd   = "</a>";
+
+            var div = divCol    +
+                            make       +
+                            odometer + 
+                            linkStart       +
+                            image       +
+                            linkEnd +
+                      divClose;
+
+            $('.col-sm-12').append(div); // insert the div you've just created
+
+        }
+
+}
+
+});
+
+}
+
+
+
+
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(setPosition);
@@ -14,12 +65,13 @@ function getLocation() {
 function setPosition(position) {
   userLatitude = position.coords.latitude;
   userLongitude = position.coords.longitude;
-  initMap();
+  
 }
 
 function initMap() {
-
-
+getAvailableCars();
+getLocation();
+  
   map = new google.maps.Map(
       document.getElementById('map'),
       {center: new google.maps.LatLng(userLatitude, userLongitude), zoom: 16});
@@ -28,6 +80,17 @@ function initMap() {
       './images/car_clipart_resized.png';
 
   
+        for(var prop in response) {
+          var contentString = 
+          '<div id="content">' + 
+          '<div id="siteNotice">' + 
+          '</div>' + 
+          '<h1 id="firstHeading" class="firstHeading">' +  + '</h1>'+
+            '<div id="bodyContent">' +
+
+
+        }
+
   var features = [
     {
       position: new google.maps.LatLng(-33.91721, 151.22630)
