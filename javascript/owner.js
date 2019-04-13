2dc35d63d5cfba42c3b678926afe3366d1333c78
TheAvianLord <angela-yixuan21@hotmail.com>
2019-04-13 16:55:52 -0700

    aMAZING THINGS

---
+++ javascript/owner.js
@@ -0,0 +1,52 @@
+var map;
+var userLatitude = 34.040800499999996;
+var userLongitude = -118.2557107;
+var availableCars;
+
+
+getAvailableCars();
+
+function getAvailableCars(){
+  console.log("Session storage: " + sessionStorage.getItem('user'));
+$.ajax({
+    url: 'https://34.212.86.167:80/registered_vehicles',
+    type: 'GET',
+    data: 
+    {
+      "email": sessionStorage.getItem('user')
+    },
+    dataType: 'json',
+    success: function(response) {
+      console.log(response);
+      availableCars = response;
+
+      var divCol  = "<div class='profile col-md-4'>";
+        
+      var divClose= "</div>";
+
+      for(var prop in response) {
+        console.log(response[prop].info);
+          var make     = "<h3>"      + response[prop].info.make + " " + response[prop].info.model  + " " + 
+            response[prop].info.year + "</h3>";
+          var odometer = "<p>" + Math.round(response[prop].odometer.data.distance).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " kilometers driven. </p>";
+          var image = "<img src='../images/" + response[prop].info.make + ".jpg' class='img-responsive pic2' />";
+          var rented     = (response[prop].rented) ? "<p style = 'font-weight:500;'> RENTED</p>" : "<p style = 'font-weight:500;'> AVAILABLE </p>";
+
+
+            var div = divCol    + image +
+                            make       +
+                            odometer + 
+                            
+                            rented       +
+                            
+                      divClose;
+
+            $('.row').append(div); // insert the div you've just created
+
+        }
+
+}
+
+});
+
+}
