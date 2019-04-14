13dbab25d47bbfab25fb811e47fb4c6c56b5b803
bhagatv <bhagatv@uci.edu>
2019-04-13 21:52:45 -0700

    dad

--- javascript/client-dashboard.js
+++ javascript/client-dashboard.js
@@ -1,4 +1,8 @@
 
+
+
+var rented_vehicles;
+
 function get_rented_vehicles()
 {
 $.ajax({
@@ -10,9 +14,45 @@ $.ajax({
     },
     dataType: 'json',
     success: function(response) {
-    	console.log(response);
+    	rented_vehicles = response;
+
+        var divCol  = "<div class='profile col-md-4'>";
+        
+        var divClose= "</div>";
+
+        for(var prop in response) {
+        
+            var make = `
+                       <div class = 'container-fluid veh-cont'> 
+                            <img src = '../images/${response[prop].info.make}.jpg' class = 'img-fluid car-pic' /> 
+                            <p class = 'vehicle_title'> ${response[prop].info.make} ${response[prop].info.model} ${response[prop].info.year} </p>
+                            <p class = 'vehicle_title'> ${Math.round(response[prop].odometer.data.distance).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} kilometers driven. </p>
+                            <div class = 'controls-cont'>
+                                <div class = 'controls'> 
+                                    <div onclick = unlock(${prop}); class = 'col-md-4 btn btn-controls btn-dark'> Unlock </div>
+                                    <div class = 'col-md-4 btn btn-controls btn-dark'> Lock </div>
+                                </div>
+                                <div class = 'controls'>
+                                    <div class = 'col-md-4 btn btn-controls btn-dark'> Return </div>
+                                    <div class = 'col-md-4 btn btn-controls btn-dark'> Test </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+
+
+
+                       `;
+              $('.container').append(make); // insert the div you've just created
+
+        }
+
+
     }
 
 });
 
+
 }
+
+get_rented_vehicles();
--- javascript/dashboard.js
+++ javascript/dashboard.js
@@ -34,7 +34,7 @@ $.ajax({
 
             response[prop].info.year + "</h3>";
           var odometer = "<p>" + Math.round(response[prop].odometer.data.distance).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " kilometers driven. </p>";
-          var image = "<img src='../images/" + response[prop].info.make + ".jpg' class='img-responsive pic2' />";
+          var image = "<img src='../images/" + response[prop].info.make + ".jpg' class=' pic2' />";
           var linkStart = "<a href='rent.html?id=" + prop + "'>";
           var image2 = "RENT NOW";
           var linkEnd = "</a>";
